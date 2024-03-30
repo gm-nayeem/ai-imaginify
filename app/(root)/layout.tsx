1875fde0a5249825803a174ab3208ac0209899e3
golam mostafa <gmnayeem25@gmail.com>
2024-03-30 23:31:10 +0600

    basic setup for the ai-imaginigy

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import MobileNav from "@/components/shared/mobile-nav";
+import Sidebar from "@/components/shared/sidebar";
+import { Toaster } from "@/components/ui/toaster";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+
+      <Toaster />
+    </main>
+  );
+};
+
+export default Layout;
---
+++ components/shared/media-uploader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MediaUploader = () => {
+  return <div>MediaUploader</div>;
+};
+
+export default MediaUploader;
